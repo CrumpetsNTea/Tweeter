94f17c5af9ddc71c8dfc77fce2a1ebc2e0da0c9e
CrumpetsNTea <t.botell@gmail.com>
2021-11-30 19:36:06 -0500

    remove all test/dev comments as per evaluator's feedback

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,22 +1,15 @@
-/* eslint-disable no-undef */
 $(document).ready(function() {
   $("#tweet-text").on('input', function(event) {
     let $numberOfChars = this.value.length;
-    // console.log(event.target.value);
     let $counter = $(this).closest("form").find(".counter").text(140 - $numberOfChars);
-    // console.log(counter);
     if ($numberOfChars > 140) {
       $counter.css("color", "red");
-      // $("#too-many-characters").slideDown();
     }
     if ($numberOfChars <= 140) {
       $counter.css("color", "#545149");
       $("#too-many-characters").slideUp();
     }
   });
-  // $("#submitTweet").submit(function(event) {
-  //   $(this).closest("form").find(".counter").text(140);
-  // }
-  // );
+
 });
 
