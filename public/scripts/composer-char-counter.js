0716988fe3a769049bfb5ef1cf5ea3dfdb2c83cf
CrumpetsNTea <t.botell@gmail.com>
2021-11-30 19:07:26 -0500

    Fixed functional requirement where character counter was not resetting to 140 after a tweet was submitted. It now resets to 140.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,13 @@
+/* eslint-disable no-undef */
 $(document).ready(function() {
-  $("#tweet-text").on('keyup', function(event) {
+  $("#tweet-text").on('input', function(event) {
     let $numberOfChars = this.value.length;
     // console.log(event.target.value);
     let $counter = $(this).closest("form").find(".counter").text(140 - $numberOfChars);
     // console.log(counter);
+    if ($numberOfChars === 0) {
+      $counter.text(140);
+    }
     if ($numberOfChars > 140) {
       $counter.css("color", "red");
       $("#too-many-characters").slideDown();
@@ -11,7 +15,10 @@ $(document).ready(function() {
     if ($numberOfChars < 140) {
       $counter.css("color", "#545149");
       $("#too-many-characters").slideUp();
-
     }
   });
+  $("#submitTweet").submit(function(event) {
+    $(this).closest("form").find(".counter").text(140);
+  }
+  );
 });
